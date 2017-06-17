5e61520168a36db069f98036dddf82e6eee74726
carlos <carlos@oak>
2017-06-16 22:38:49 -0300

    -

--- Python/pass.py
+++ Python/pass.py
@@ -14,18 +14,8 @@ def generate(lenght):
     current_number = 1
     while current_number <= lenght:
         password.append(random.choice(string.digits + string.ascii_lowercase + string.digits + string.ascii_lowercase))
-        try:
-            if len(item) == len(set(item)):
-
         current_number += 1
     return (password)
 
+print(generate(lenght))
 
-
-#        if len(password) == len(set(password)):
-        #    print("q")
-
-
-generate(lenght)
-
-print(password)
