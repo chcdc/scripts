6db5e3a9b82823a764c15e48b1830408695f09d3
carlos <carlos@04k.lan>
2017-12-31 20:52:39 -0200

    Usage check IP Amazon AWS

--- Python/ip_externo.py
+++ Python/ip_externo.py
@@ -6,7 +6,7 @@
 
 import requests
 
-r = requests.get('http://ifconfig.me/ip')
+r = requests.get('http://checkip.amazonaws.com')
 print("#" * 33 )
 print("\n")
 print("Seu ip externo é: {0}".format(r.text))
